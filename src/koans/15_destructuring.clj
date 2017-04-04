6be97c27cb32fe1b803c7fbc23f384175b9621dc
ofadeyi <ofadeyi@gmail.com>
2017-04-04 12:45:11 +0100

    KOANS-15 | Ola | Completed

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -15,30 +15,38 @@
   (= (str "An Oxford comma list of apples, "
           "oranges, "
           "and pears.")
-     ((fn [[a b c]] (let [x (interpose "," [a b c])] (str "An Oxford comma list of " x ".")))
+    ((fn [[a b c]] (let [y (str "and " c)
+                         x (apply str (interpose ", " [a b y]))]
+                     (str "An Oxford comma list of " x ".")))
       ["apples" "oranges" "pears"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
-     (let [[first-name last-name & aliases]
-           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       __))
+    (let [[first-name last-name & aliases]
+          (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
+      (let [sep " aka "]
+        (str first-name " " last-name sep (apply str (interpose sep aliases))))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       (assoc {} :original-parts full-name
+                 :named-parts (assoc {} :first first-name :last last-name))))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+       (apply str (interpose ", " [street-address city state]))))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (apply str (interpose ", " [street-address city state]))))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [[test testerson] address]
+         (let [name (str test " " testerson)
+               {:keys [street-address city state]} address]
+           (apply str (interpose ", " [name street-address city state]))))
+       ["Test" "Testerson"] test-address)))
